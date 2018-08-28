b985d374eb83119232974af3fc88b16d946c9049
CANDRA RAMADHAN <candraramadhanp@gmail.com>
2018-08-28 23:53:52 +0800

    Initial commit

---
+++ app/src/main/java/com/kardusinfo/array2recyclerview/Item.kt
@@ -0,0 +1,3 @@
+package com.kardusinfo.array2recyclerview
+
+data class Item (val name: String?, val image: Int?)
---
+++ app/src/main/java/com/kardusinfo/array2recyclerview/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.kardusinfo.array2recyclerview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private var items: MutableList<Item> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val list = findViewById<RecyclerView>(R.id.club_list)
+        initData()
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this, items){
+            val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
+            toast.show()
+        }
+     }
+    private fun initData(){
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+        items.clear()
+        for (i in name.indices) {
+            items.add(Item(name[i],
+                    image.getResourceId(i, 0)))
+        }
+
+        //Recycle the typed array
+        image.recycle()
+    }
+}
